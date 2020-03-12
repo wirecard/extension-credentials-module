818db641278a4d83ad17b3370729b50be55ee0d1
ozada <oskar.huseynzada@wirecard.com>
2020-03-12 11:44:17 +0100

    Add licence header to each *.php file.

--- tests/Exception/InvalidPaymentMethodExceptionTest.php
+++ tests/Exception/InvalidPaymentMethodExceptionTest.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * Shop System Extensions:
+ * - Terms of Use can be found at:
+ * https://github.com/wirecard/extension-credentials-module/blob/master/_TERMS_OF_USE
+ * - License can be found under:
+ * https://github.com/wirecard/extension-credentials-module/blob/master/LICENSE
+ */
 
 namespace WirecardTest\Exception;
 
